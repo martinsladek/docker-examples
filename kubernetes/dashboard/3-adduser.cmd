0387205acca5db1b5636b35966ef0388b812b0ce
Martin Sladek <martinsladek@users.noreply.github.com>
2023-03-11 16:41:57 +0100

    Kubernetes - Dashboard UI - scripts clean up

--- kubernetes/dashboard/3-adduser.cmd
+++ kubernetes/dashboard/3-adduser.cmd
@@ -1,5 +1,5 @@
-kubectl apply -f 1-kubernetes-adminuser.yaml
-kubectl apply -f 2-kubernetes-adminuser.yaml
+kubectl apply -f files/1-kubernetes-adminuser.yaml
+kubectl apply -f files/2-kubernetes-adminuser.yaml
 kubectl -n kubernetes-dashboard create token admin-user > admin-user-token.txt
 start notepad admin-user-token.txt
 @rem echo copy admin token to your browser window

6cdbd0d6eca92f2964e82d7768d4586115ef3909
Martin Sladek <martinsladek@users.noreply.github.com>
2023-03-11 16:25:28 +0100

    Kubernetes - Dashboard UI - scripts updated

--- kubernetes/dashboard/3-install.cmd
+++ kubernetes/dashboard/3-install.cmd
@@ -1,6 +1,7 @@
 kubectl apply -f 1-kubernetes-adminuser.yaml
 kubectl apply -f 2-kubernetes-adminuser.yaml
-kubectl -n kubernetes-dashboard create token admin-user
-echo copy admin token to your browser window
-echo save token as password
+kubectl -n kubernetes-dashboard create token admin-user > admin-user-token.txt
+start notepad admin-user-token.txt
+@rem echo copy admin token to your browser window
+@rem echo save token as password
 pause

e3bafa59b4a73a22630d4d03a4a090d2870dfa10
Martin Sladek <martinsladek@users.noreply.github.com>
2023-03-11 16:42:54 +0100

    Kubernetes - Dashboard UI - scripts clean up

---
+++ kubernetes/dashboard/2-start.cmd
@@ -0,0 +1,3 @@
+@rem kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+kubectl apply -f files/kubernetes-dashboard-recommended.yaml
+@rem echo do not forget to start: kubectl proxy
